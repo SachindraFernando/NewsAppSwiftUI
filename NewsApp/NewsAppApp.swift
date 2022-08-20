f79853606cd5f637c6d00850c052337234acd644
Sachindra <sachindrac.emedia@gmail.com>
2022-08-21 00:12:45 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Sachindra Fernando on 2022-08-21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
